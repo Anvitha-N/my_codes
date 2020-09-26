dfbf009a79ca15b029c9142e36f1436561fcc3a0
Anvitha-N <68950645+Anvitha-N@users.noreply.github.com>
2020-09-26 15:41:29 +0530

    Add files via upload
    small programs on python

---
+++ my fibonacci.py
@@ -0,0 +1,26 @@
+#python program for fibonacci numbers
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <=0:
+    print("please enter apositve integer")
+elif nterms == 1:
+    print("Fibonacci sequence upto",nterms,":")
+    print(n1)
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+
+        #update values
+        n1 = n2
+        n2 = nth
+        count +=1
+        
+
